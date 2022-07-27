c62e1a42e7e4234c653eb2291b5609516adb2e17
Tomás Caldas <tomas.farias@icloud.com>
2022-07-27 12:15:38 -0300

    ADD M4A11

---
+++ 1 - CursoLogicadeProgramaçãoeOrientaçãoaObjetos/Modulo04-Programação Orientada a objetos/1 - Atividades/Aula11/M4A11py.py
@@ -0,0 +1,14 @@
+#Aluno: Tomás de Farias Ribeiro Caldas
+#M4A11
+
+#Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+
+frase = 'Tenha esperança pois a sua hora vai chegar'
+print('A quantidade de letras a é:', frase.count('a'))
+
+resultado = 'estarei entre os trezentos selecionados'
+print('A quantidade de caracteres é:', len(resultado.strip()))
+
+aprimorando = 'tenho certeza que irei ser reprovado!'
+print(aprimorando.replace('reprovado', 'aprovado'))
